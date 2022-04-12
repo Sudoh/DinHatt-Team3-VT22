7e382519383d0210f7143eaed639223ba1f0f1b3
Payam K <payam@karimian.se>
2022-04-12 13:57:33 +0200

    Startup

---
+++ DinHatt-CodeFirst/Program.cs
@@ -0,0 +1,32 @@
+﻿using Models;
+using System;
+using System.Data.Entity;
+using System.Windows.Forms;
+
+namespace DinHatt_CodeFirst
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new StartMenyForm());
+        }
+    }
+
+
+    public class DinHatt : DbContext
+    {
+        //Här initialiseras alla tabeller i databasen när program körs. 
+        //Finns det något här så skapas det en tabell.
+        public DbSet<Artikel> Artiklar { get; set; }
+        public DbSet<Order> Ordrar { get; set; }
+        public DbSet<Kund> Kunder { get; set; }
+
+    }
+}
---
+++ DinHatt-CodeFirst/StatistikMeny/StatistikForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace DinHatt_CodeFirst
+{
+    public partial class StatistikForm : Form
+    {
+        public StatistikForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StatistikForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+    }
+}
