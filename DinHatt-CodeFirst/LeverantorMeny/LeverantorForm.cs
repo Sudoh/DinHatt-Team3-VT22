845712cbe294d0eb2769a5e4b59588602d556db9
Diyar9 <83557393+Diyar9@users.noreply.github.com>
2022-04-25 13:19:01 +0200

    Hämta leverantör + DB<Leverantör> = klar

--- DinHatt-CodeFirst/LeverantorMeny/LeverantorForm.cs
+++ DinHatt-CodeFirst/LeverantorMeny/LeverantorForm.cs
@@ -16,5 +16,34 @@ namespace DinHatt_CodeFirst.LeverantorMeny
         {
             InitializeComponent();
         }
+
+        private void btnVisaLeverantorer_Click(object sender, EventArgs e)
+        {
+            listLeverantor.BeginUpdate();
+            listLeverantor.Items.Clear();
+
+            using (var db = new DinHatt())
+            {
+
+                foreach (var item in db.Leverantorer)
+                {
+                    ListViewItem rad = new ListViewItem();
+
+                    rad.SubItems.Add(item.foretagsId.ToString());
+                    rad.SubItems.Add(item.foretagsNamn);
+                    rad.SubItems.Add(item.foretagsAdress);
+                    rad.SubItems.Add(item.foretagsPostNr.ToString());
+                    rad.SubItems.Add(item.foretagsOrt);
+                    rad.SubItems.Add(item.foretagsLand);
+                    rad.SubItems.Add(item.foretagsTele);
+                    rad.SubItems.Add(item.foretagsEpost);
+
+                    listLeverantor.Items.Add(rad);
+                }
+
+                listLeverantor.EndUpdate();
+                listLeverantor.Refresh();
+            }
+        }
     }
 }
